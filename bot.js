9461a07d7b0380b6ec23f80f0eb4b36c5cd217e0
SirNightmare <38385282+SirNightmare@users.noreply.github.com>
2018-06-08 17:19:30 +0100

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,47 @@
+// For v11 and above only.
+
+const Discord = require('discord.js');
+const Music = require("discord.js-musicbot-addon")
+var settings = require("./settings.json")
+
+class Bot extends Discord.Client {
+  constructor(options) {
+    super(options);
+    this.music = require('discord.js-musicbot-addon');
+    this.config = require('./settings.json');
+  }
+}
+
+const client = new Bot();
+
+client.on('ready', () => {
+    console.log(`[Start] ${new Date()}`);
+});
+
+Music.start(client, {
+  youtubeKey: "AIzaSyChV72AqgUOWab694WT3zdK6EIbY0EGRuc",
+   prefix: "?", 
+    helpCmd: "mhelp",
+    global: false, 
+    maxQueueSize: 50,
+    playCmd: 'music',  
+    playAlts: ["music", 'moosic'],
+    volumeCmd: 'vol', 
+    thumbnailType: 'high',
+    leaveCmd: 'leave',
+    anyoneCanSkip: true, 
+    disableLoop: false,
+    searchCmd: 'search',
+    requesterName: true,
+    inlineEmbeds: true,     
+    queueCmd: 'queue',
+    queueAlts: ['queue', 'queueue'],
+    pauseCmd: 'pause',
+    resumeCmd: 'resume',
+    skipCmd: 'skip',
+    skipAlts: ["skip", "skipp"],
+    loopCmd: 'loop',
+    enableQueueStat: true,
+});
+
+client.login(settings.token);
